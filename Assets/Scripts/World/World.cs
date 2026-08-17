587ab2191096dc06651a449840597a7570644f5c
Piper Inns Hall <piper.innshall@gmail.com>
2026-08-17 16:48:31 +1200

    chore: Add callback for scene loading

--- Assets/Scripts/World/World.cs
+++ Assets/Scripts/World/World.cs
@@ -4,18 +4,18 @@ using UnityEngine.SceneManagement;
 
 public class World {
   private GameManager _game = new GameManager();
-  public void Start() { _game.Transition(new GameManager.State.Menu()); }
+  public void Start() { _game.Transition(new GameManager.States.Loading()); }
 }
 
 public class GameManager {
-  private State _state = new State.Menu();
-  public State State => _state;
+  private States _state = new States.Menu();
+  public States State => _state;
 
-  public abstract record State {
-    public sealed record Menu() : State;
-    public sealed record Loading() : State;
-    public sealed record Playing() : State;
-    public sealed record Dead() : State;
+  public abstract record States {
+    public sealed record Menu() : States;
+    public sealed record Loading() : States;
+    public sealed record Playing() : States;
+    public sealed record Dead() : States;
     public override string ToString() => this switch {
       Menu => "Menu",
       Loading => "Loading",
@@ -25,17 +25,18 @@ public class GameManager {
     };
   }
 
-  public void Transition(State to) { _state = Transition(_state, to); }
-  private State Transition(State state, State to) => (state, to) switch {
-    (State.Menu, State.Loading) => LoadWorld(to),
-    (State.Loading, State.Playing) => to,
-    (State.Playing, State.Dead) => to,
-    (State.Dead, State.Loading) => to,
+  public void Transition(States to) => _state = Transition(_state, to);
+
+  private States Transition(States state, States to) => (state, to) switch {
+    (States.Menu, States.Loading) => LoadWorld(to),
+    (States.Loading, States.Playing) => to,
+    (States.Playing, States.Dead) => to,
+    (States.Dead, States.Loading) => to,
     _ => throw new InvalidOperationException("Invalid transition")
   };
 
-  private State LoadWorld(State state) {
-    Debug.Log("Loading World");
+  private States LoadWorld(States state) {
+    SceneManager.sceneLoaded += (scene, mode) => Transition(new States.Playing());
     SceneManager.LoadSceneAsync("SampleScene");
     return state;
   }
