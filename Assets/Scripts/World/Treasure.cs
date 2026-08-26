145a359aaf4ae83038556824ac433e3571aa11e7
Piper Inns Hall <piper.innshall@gmail.com>
2026-08-26 23:10:28 +1200

    feat:  Make treasure state JSON compliant

--- Assets/Scripts/World/Treasure.cs
+++ Assets/Scripts/World/Treasure.cs
@@ -1,36 +1,65 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public record TreasureState(
   string Id,
   bool IsOpened,
-  IReadOnlyList<ICoinPacket> Packets,
+  IReadOnlyList<ICoinPacket> Packets
 );
 
 public interface ITreasure {
-  string Id { get; }
-  bool IsOpened { get; }
   TreasureState State { get; }
+  void Initialize(TreasureManager manager);
   void Add(ICoinPacket packet);
   void Open();
 }
 
-public class Treasure<T> : MonoBehaviour, ITreasure where T : ICoinPacket {
-  [SerializeField] private string _id;
+public class Treasure<T> : ITreasure where T : ICoinPacket {
+  private string _id;
+  private bool _isOpened;
   private readonly List<ICoinPacket> _packets = new();
+  private TreasureManager _manager;
 
-  public string Id => _id;
-  public bool IsOpened { get; private set; }
-  public TreasureState State => new(Id, IsOpened, _packets.ToList());
+  public TreasureState State => new(_id, _isOpened, _packets.AsReadOnly());
 
-  private void Awake() => _packets.Add(ICoinPacket.Create<T>());
+  public Treasure(string id) {
+    _id = id;
+    _packets.Add(ICoinPacket.Create<T>());
+  }
+
+  public void Initialize(TreasureManager manager) => _manager = manager;
   public void Add(ICoinPacket packet) => _packets.Add(packet);
-  public void Open() => IsOpened = true;
+
+  public void Open() {
+    if (_isOpened) return;
+    _isOpened = true;
+    _manager.Open(TakePackets());
+  }
+
+  private List<ICoinPacket> TakePackets() {
+    var packets = _packets.ToList();
+    _packets.Clear();
+    return packets;
+  }
 }
 
-// Unity needs concrete MonoBehaviour types for GameObject's in the Inspector.
-public class CheapTreasure : Treasure<ICoinPacket.Cheap> {}
-public class ModerateTreasure : Treasure<ICoinPacket.Moderate> {}
-public class ValuableTreasure : Treasure<ICoinPacket.Valuable> {}
-public class SacredTreasure : Treasure<ICoinPacket.Sacred> {}
+/*
+ * var views = FindObjectsOfType<TreasureView>();
+ * foreach (var view in treasureViews) {
+ *   view.Initialize(manager.Treasures.Single(t => t.Id == view.Id));
+ * }
+ *
+ * public class TreasureView : MonoBehaviour {
+ *   private ITreasure _treasure;
+ *   public void Initialize(ITreasure treasure) => _treasure = treasure;
+ *   public void Open() {
+ *     _treasure.Open();
+ *     PlayOpeningAnimation();
+ *   }
+ *   private void PlayOpeningAnimation() {
+ *     // Animator.Play(...)
+ *   }
+ * }
+ */
--- Assets/Scripts/World/TreasureManager.cs
+++ Assets/Scripts/World/TreasureManager.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 
 public record TreasureManagerState(
   IReadOnlyList<ICoinPacket> Packets,
+  IReadOnlyList<TreasureState> Treasures
 );
 
 public class TreasureManager {
-  private List<ICoinPacket> _packets = new();
-  public int GetTotalCoins() => _packets.Sum(p => p.Amount);
-  public void Add(ICoinPacket packet) => _packets.Add(packet);
-  public void Remove(ICoinPacket packet) => _packets.Remove(packet);
+  private readonly List<ICoinPacket> _packets = new();
+  private List<ITreasure> _treasures = new();
 
-  public void OnPlayerDeath(List<ITreasure> allTreasures) {
-    var closed = allTreasures.Where(t => !t.IsOpened).ToList();
-    if (closed.Count == 0) return;
-    foreach (var (p, i) in _packets.Enumerate()) closed[i % closed.Count].Add(p);
+  public TreasureManagerState State =>
+    new(_packets.ToList(), _treasures.Select(t => t.State).ToList());
+
+  public void Initialize(IReadOnlyList<ITreasure> treasures) => 
+    _treasures = treasures.ToList();
+
+  public int GetTotalCoins() => _packets.Sum(p => p.Amount);
+
+  public void Open(List<ICoinPacket> packets) => _packets.AddRange(packets);
+
+  public void Redistribute() {
+    var closed = _treasures.Where(t => !t.State.IsOpened).ToList();
+    if (!closed.Any()) return;
+    foreach (var (p, i) in _packets.Enumerate()) closed[i % closed.Count()].Add(p);
     _packets.Clear();
   }
 }
@@ -29,11 +38,11 @@ public interface ICoinPacket {
   public sealed record Sacred(int Amount) : ICoinPacket;
   public static ICoinPacket Create<T>() where T : ICoinPacket {
     return typeof(T) switch {
-        var t when t == typeof(Cheap) => new Cheap(UnityEngine.Random.Range(1, 6)),
-        var t when t == typeof(Moderate) => new Moderate(UnityEngine.Random.Range(7, 14)),
-        var t when t == typeof(Valuable) => new Valuable(UnityEngine.Random.Range(15, 21)),
-        var t when t == typeof(Sacred) => new Sacred(UnityEngine.Random.Range(22, 27)),
-        _ => throw new ArgumentException($"Unknown packet type: {typeof(T)}")
+      var t when t == typeof(Cheap) => new Cheap(UnityEngine.Random.Range(1, 6)),
+      var t when t == typeof(Moderate) => new Moderate(UnityEngine.Random.Range(7, 14)),
+      var t when t == typeof(Valuable) => new Valuable(UnityEngine.Random.Range(15, 21)),
+      var t when t == typeof(Sacred) => new Sacred(UnityEngine.Random.Range(22, 27)),
+      _ => throw new ArgumentException($"Unknown packet type: {typeof(T)}")
     };
   }
 }
